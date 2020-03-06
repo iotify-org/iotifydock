53f23bfb6330725e47d1cc2077b261296d2192f6
iotify-org <benja.sanz@iotify.org>
2020-03-06 18:24:40 -0300

    commit

--- install.sh
+++ install.sh
@@ -1,37 +1,33 @@
-##!/bin/bash
+#!/bin/bash
+
 ## ------------------------------------------------------------------
 ## [Author] Title
 ##          Description
 ## ------------------------------------------------------------------
-# 
-#cd /
-#
-#apt-get update
-#apt dist-upgrade -y
-#apt install fail2ban -y
-#
-##DOCKER
-#apt-get install -y \
-#    apt-transport-https \ 
-#    ca-certificates \
-#    curl \
-#    gnupg-agent \
-#    software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#apt-key fingerprint 0EBFCD88
-#apt-get update.
-#apt-get install docker-ce docker-ce-cli containerd.io -y
-#docker run hello-world
-#
-#
-##DOCKER COMPOSE
-#curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-$
-#chmod +x /usr/local/bin/docker-compose
-#docker-compose --version
-#
-##IOTIFY
-#git clone https://github.com/iotify-org/iotifydock.git
-#cd iotifydock
+clear
+msg="
+  _____     _____ _  __       
+  \_   \___/__   (_)/ _|_   _ 
+   / /\/ _ \ / /\/ | |_| | | |
+/\/ /_| (_) / /  | |  _| |_| |
+\____/ \___/\/   |_|_|  \__, |
+                        |___/ 
+
+"
+tput setaf 4;
+echo "$msg"
+tput setaf 7;
+
+random_string()
+{
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+}
+sleep 2
+
+tput setaf 2;
+echo "We will need some information to configure the system installation"
+tput setaf 7;
+echo ""
 
 while [[ -z "$timezone" ]]
 do
@@ -51,7 +47,7 @@ done
 
 while [[ -z "$mysql_root_password" ]]
 do
-  read -p "Enter a new root mysql user password : " mysql_root_password
+  read -p "Enter a new root mysql password : " mysql_root_password
 done
 
 
@@ -63,7 +59,7 @@ done
 
 while [[ -z "$mysql_admin_password" ]]
 do
-  read -p "Enter a new admin  mysql user password : " mysql_admin_password
+  read -p "Enter a new admin mysql password : " mysql_admin_password
 done
 
 while [[ -z "$mysql_database_name" ]]
@@ -71,15 +67,109 @@ do
   read -p "Enter a name for mysql database : " mysql_database_name
 done
 
-while [[ -z "$emqx_dash_user" ]]
-do
-  read -p "Enter a username for EMQX dashboard : " emqx_dash_user
-done
 
-while [[ -z "$emqx_dash_password" ]]
-do
-  read -p "Enter a password for EMQX dashboard : " emqx_dash_password
-done
+
+echo ""
+echo ""
+echo ""
+tput setaf 2;
+echo "*************************"
+echo "**** S U M A R Y ********"
+echo "*************************"
+tput setaf 7;
+echo ""
+echo "TIMEZONE: $timezone"
+echo "DOMAIN: $domain"
+echo "EMAIL SSL: $email"
+echo ""
+tput setaf 4;
+echo "if you see any error press ctrl + c"
+tput setaf 7;
+
+for i in {10..1..1};do echo -n "$i." && sleep 1; done
+
+
+emqx_dash_user=admin
+emqx_dash_password=$(random_string)
+emqx_api_id=admin
+emqx_api_secret=$(random_string)
+
+
+
+cd /
+
+tput setaf 2;
+echo ""
+echo "**************************"
+echo "**** UPDATING SYSTEM *****"
+echo "**************************"
+echo ""
+tput setaf 7;
+sleep 3
+
+
+apt-get update
+apt dist-upgrade -y
+
+tput setaf 2;
+echo ""
+echo "******************************"
+echo "**** INSTALLING FAIL2BAN *****"
+echo "******************************"
+echo ""
+tput setaf 7;
+sleep 3
+
+apt install fail2ban -y
+
+tput setaf 2;
+echo ""
+echo "******************************"
+echo "**** INSTALLING DOCKER *****"
+echo "******************************"
+echo ""
+tput setaf 7;
+sleep 3
+
+#DOCKER
+apt-get install -y \
+    apt-transport-https \ 
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+apt-get update.
+apt-get install docker-ce docker-ce-cli containerd.io -y
+docker run hello-world
+
+tput setaf 2;
+echo ""
+echo "************************************"
+echo "**** INSTALLING DOCKER-COMPOSE *****"
+echo "************************************"
+echo ""
+tput setaf 7;
+sleep 3
+
+#DOCKER COMPOSE
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-$
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+tput setaf 2;
+echo ""
+echo "******************************"
+echo "**** CLONING REPOSITORY ******"
+echo "******************************"
+echo ""
+tput setaf 7;
+sleep 3
+
+#IOTIFY
+git clone https://github.com/iotify-org/iotifydock.git
+cd iotifydock
 
 
 echo "TIMEZONE=$timezone" > ./.env
@@ -105,10 +195,30 @@ echo "MYSQL_DATABASE=$mysql_database_name" >> ./.env
 
 echo "EMQX_DASH_USER=$emqx_dash_user" >> ./.env
 echo "EMQX_DASH_PASS=$emqx_dash_password" >> ./.env
+echo "EMQX_API_ID=$emqx_api_id" >> ./.env
+echo "EMQX_API_SECRET=$emqx_api_secret" >> ./.env
+
+tput setaf 2;
+echo ""
+echo "*******************************"
+echo "**** GENERATING SSL CERTS *****"
+echo "*******************************"
+echo ""
+tput setaf 7;
+sleep 3
 
 #GENERATING SSL CERTS
-#docker-compose -f certbot.yml up
-#docker rm $(docker ps -a -q) -f
+docker-compose -f certbot.yml up
+docker rm $(docker ps -a -q) -f
+
+tput setaf 2;
+echo ""
+echo "*************************************"
+echo "****   INSTALLING MQTT BROKER   *****"
+echo "*************************************"
+echo ""
+tput setaf 7;
+sleep 3
 
 unzip emqx.zip
 
@@ -121,6 +231,10 @@ sed -i "460s/.*/allow_anonymous = false/" ./emqx/etc/emqx.conf
 sed -i "465s/.*/acl_nomatch = deny/" ./emqx/etc/emqx.conf
 
 #SETTING UP MQTT LISTENERS
+sed -i "880s/.*/listener.tcp.external.max_connections = 100/" ./emqx/etc/emqx.conf
+sed -i "885s/.*/listener.tcp.external.max_conn_rate = 25/" ./emqx/etc/emqx.conf
+sed -i "1011s/.*/listener.tcp.internal.max_connections = 100/" ./emqx/etc/emqx.conf
+sed -i "1121s/.*/listener.ssl.external.max_conn_rate = 25/" ./emqx/etc/emqx.conf
 sed -i "1116s/.*/listener.ssl.external.max_connections = 100/" ./emqx/etc/emqx.conf
 sed -i "1121s/.*/listener.ssl.external.max_conn_rate = 25/" ./emqx/etc/emqx.conf
 sed -i "1367s/.*/listener.ws.external.max_connections = 100/" ./emqx/etc/emqx.conf
@@ -129,14 +243,29 @@ sed -i "1578s/.*/listener.wss.external.max_connections = 100/" ./emqx/etc/emqx.c
 sed -i "1585s/.*/listener.wss.external.max_conn_rate = 25/" ./emqx/etc/emqx.conf
 
 #SETTING UP MAIN EMQX API CREDENTIALS
-sed -i "16s/.*/management.default_application.id = $emqx_dash_user/" ./emqx/etc/plugins/emqx_management.conf
-sed -i "21s/.*/management.default_application.secret = $emqx_dash_password/" ./emqx/etc/plugins/emqx_management.conf
+sed -i "16s/.*/management.default_application.id = $emqx_api_id/" ./emqx/etc/plugins/emqx_management.conf
+sed -i "21s/.*/management.default_application.secret = $emqx_api_secret/" ./emqx/etc/plugins/emqx_management.conf
+
 
 #SETTING UP EMQX AUTH MYSQL
+sed -i "10s/.*/auth.mysql.server = db:3306/" ./emqx/etc/plugins/emqx_auth_mysql.conf
 sed -i "20s/.*/auth.mysql.username = $mysql_admin_user/" ./emqx/etc/plugins/emqx_auth_mysql.conf
 sed -i "25s/.*/auth.mysql.password = $mysql_admin_password/" ./emqx/etc/plugins/emqx_auth_mysql.conf
+sed -i "30s/.*/auth.mysql.database = $mysql_database_name/" ./emqx/etc/plugins/emqx_auth_mysql.conf
 sed -i "35s/.*/auth.mysql.query_timeout = 15s/" ./emqx/etc/plugins/emqx_auth_mysql.conf
 sed -i "96s/.*/auth.mysql.acl_query = select allow, ipaddr, username, clientid, access, topic from mqtt_user_acl where ipaddr = '%a' or username = '%u' or username = '$all' or clientid = '%c' ORDER BY id ASC/" ./emqx/etc/plugins/emqx_auth_mysql.conf
+
+#SETTING UP DEFAULT EMQX PLUGINS 
+echo '{emqx_auth_mysql,true}.' >> ./emqx/data/loaded_plugins
+
+tput setaf 2;
+echo ""
+echo "**********************************"
+echo "****   INSTALLING MQTT SSL   *****"
+echo "**********************************"
+echo ""
+tput setaf 7;
+sleep 3
 
 rm ./emqx/etc/certs/cert.pem
 rm ./emqx/etc/certs/key.pem
@@ -144,3 +273,23 @@ rm ./emqx/etc/certs/key.pem
 ln -s ./user-data/etc/letsencrypt/live/$domain/cert.pem ./emqx/etc/certs/cert.pem
 ln -s ./user-data/etc/letsencrypt/live/$privkey/key.pem ./emqx/etc/certs/key.pem
 
+tput setaf 2;
+echo ""
+echo "**********************************"
+echo "****    STARTING SERVICES    *****"
+echo "**********************************"
+echo ""
+tput setaf 7;
+sleep 3
+
+docker-compose up -d
+
+tput setaf 2;
+echo ""
+echo "**********************************"
+echo "****         READY!!!        *****"
+echo "**********************************"
+echo ""
+tput setaf 7;
+
+echo "Go to https://$domains that's all over here..."
